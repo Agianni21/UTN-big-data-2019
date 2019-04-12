4d352c29f263a623b8dbb8959539035da456808c
Agustin Borello <agustinborello21@gmail.com>
2019-04-12 13:22:10 -0300

    Mongito

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testCompile("junit", "junit", "4.12")
     implementation("org.twitter4j", "twitter4j-core", "4.0.7")
+    implementation("org.mongodb", "mongodb-driver-sync", "3.10.1")
 }
 
 configure<JavaPluginConvention> {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,45 @@
+import com.mongodb.client.*;
+import org.bson.Document;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
+import com.mongodb.ConnectionString;
+
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main {
     static public void main(String[] args) {
+        String searchWord = "futbol";
+        String filterRegex = "[Cc]lub";
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        // Conexion con mongito
+        MongoClient mongoClient = MongoClients.create();
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+        MongoCollection<Document> collection = database.getCollection("test");
+
+        /* FILTRO
+        FindIterable<Document> queryResult = collection.find(regex("text", "[Cc]lub"));
+        for (Document doc:queryResult) {
+            System.out.println(doc.toJson());
+        }
+
+         */
+
+        // Todo lo que es twitter
+        /*
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
                 .setOAuthConsumerKey("4vfqDOhQe2onAdeTKL5FFq8lK")
@@ -11,17 +48,22 @@ public class Main {
                 .setOAuthAccessTokenSecret("BB0IWfFu2LlCwj0hRlJsckw7rvpAj2tmykK6hHIa448OI");
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
-        Query query = new Query("macri");
+        Query query = new Query(searchWord);
         query.count(100);
         try {
             QueryResult result = twitter.search(query);
+            List<Document> documents = new ArrayList<Document>();
             for (Status status: result.getTweets()) {
-                System.out.println("@" + status.getUser().getScreenName() + ":" +  status.getText());
+                Document document = new Document("user", status.getUser().getScreenName())
+                        .append("text", status.getText());
+                documents.add(document);
             }
+            collection.insertMany(documents);
         } catch (Exception e) {
             System.out.println("Ooops..");
             System.out.println(e);
         }
 
+         */
     }
 }
