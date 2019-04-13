35fe2ca185566575417638024848c060fd1b35f2
Agustin Gianni Borello <agustinborello21@gmail.com>
2019-04-12 21:24:22 -0300

    Refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import com.mongodb.client.*;
 import org.bson.Document;
 import twitter4j.*;
+import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 import com.mongodb.ConnectionString;
 
@@ -20,15 +21,13 @@ import java.util.List;
 
 
 public class Main {
+
+    Twitter twitter;
+
     static public void main(String[] args) {
         String searchWord = "futbol";
         String filterRegex = "[Cc]lub";
-        System.out.println(args[0]);
-        System.out.println(args[1]);
         // Conexion con mongito
-        MongoClient mongoClient = MongoClients.create();
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        MongoCollection<Document> collection = database.getCollection("test");
 
         /* FILTRO
         FindIterable<Document> queryResult = collection.find(regex("text", "[Cc]lub"));
@@ -37,33 +36,63 @@ public class Main {
         }
 
          */
+    }
 
-        // Todo lo que es twitter
-        /*
+    private void scrapeTweetsAndStoreThem(String searchPhrase) {
+        twitter = createTwitterInstance();
+        Query query = buildTwitterQuery(searchPhrase);
+        QueryResult result = processQuery(query);
+        MongoCollection collection = getMongoCollection();
+        storeQueryResult(result, collection);
+    }
+
+    private Twitter createTwitterInstance() {
+        Configuration configuration = createTwitterInstanceConfiguration();
+        TwitterFactory tf = new TwitterFactory(configuration);
+        Twitter twitter = tf.getInstance();
+        return twitter;
+    }
+
+    private Configuration createTwitterInstanceConfiguration() {
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
                 .setOAuthConsumerKey("4vfqDOhQe2onAdeTKL5FFq8lK")
                 .setOAuthConsumerSecret("dXsMHh1oYq3DcpXq6MEMxkYUD74r0zOXIoRfT3tEoO5d2poDaB")
                 .setOAuthAccessToken("3108911170-iB2sAGhGuF2HZAYVQsRII8qL3dtwR7MHgN3Mo2e")
                 .setOAuthAccessTokenSecret("BB0IWfFu2LlCwj0hRlJsckw7rvpAj2tmykK6hHIa448OI");
-        TwitterFactory tf = new TwitterFactory(cb.build());
-        Twitter twitter = tf.getInstance();
-        Query query = new Query(searchWord);
+        Configuration configuration = cb.build();
+        return configuration;
+    }
+
+    private Query buildTwitterQuery(String searchPhrase) {
+        Query query = new Query(searchPhrase);
         query.count(100);
+        return query;
+    }
+
+    private QueryResult processQuery(Query query) {
         try {
-            QueryResult result = twitter.search(query);
-            List<Document> documents = new ArrayList<Document>();
-            for (Status status: result.getTweets()) {
-                Document document = new Document("user", status.getUser().getScreenName())
-                        .append("text", status.getText());
-                documents.add(document);
-            }
-            collection.insertMany(documents);
+            return twitter.search(query);
         } catch (Exception e) {
-            System.out.println("Ooops..");
+            System.out.println("----------Query failed----------");
             System.out.println(e);
         }
+    }
 
-         */
+    private void storeQueryResult(QueryResult result, MongoCollection collection) {
+        List<Document> documents = new ArrayList<Document>();
+        for (Status status : result.getTweets()) {
+            Document document = new Document("user", status.getUser().getScreenName())
+                    .append("text", status.getText());
+            documents.add(document);
+        }
+        collection.insertMany(documents);
+    }
+
+    private MongoCollection getMongoCollection() {
+        MongoClient mongoClient = MongoClients.create();
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+        MongoCollection<Document> collection = database.getCollection("test");
+        return collection;
     }
 }
